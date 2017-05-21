9fa43e00e8ab249de70cd63c7dbf5397c87a00f8
hucong <hucong@jd.com>
2017-05-21 23:37:19 +0800

    update

--- preloadimg.js
+++ preloadimg.js
@@ -1,16 +1,17 @@
+/**!
+*  A library for preload images.
+*  @author pspgbhu <brotherchun001@gmail.com>
+*  @param  {Array}     srcAry  The array of image src.
+*  @param  {function}  success success callback.
+*  @param  {funciton}  fail    fail callback.
+*/
+
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global.preloadimg = factory());
 }(this, (function () { 'use strict';
-
-/**!
- *  A library for preload images.
- *  @author pspgbhu <brotherchun001@gmail.com>
- *  @param  {Array}     srcAry  The array of image src.
- *  @param  {function}  success success callback.
- *  @param  {funciton}  fail    fail callback.
- */
 
 function preloadimg(srcAry, success, fail) {
   var precent = 0;
